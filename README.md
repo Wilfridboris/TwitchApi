d37c8dc66fe70bd57588a53f2aa3b3f7891a1298
Boris Kwayep <Boriskwayep35@yahoo.com>
2021-07-29 00:11:21 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# TwitchApi
-Real Time twitch Api
+# Atecna - Test Developer Web Fullstack JS
+
+For this project, you will be fetching and displaying data from the Twitch API.
+You can find the documentation for the Twitch API [here](https://dev.twitch.tv/docs/api)
+
+> Please note that in order to access the twitch API you'll have to create a Twitch account.
+
+**Objective**
+
+Your goal will be to build a simple web dashboard with the following 2 features:
+
+You will need to create a Web Socket connection to display the number of viewers for Chess (https://www.twitch.tv/directory/game/Chess) that automatically updates a counter in real time (*keep in mind the request throttling limit of the Twitch API*).
+
+We would like a line Chart that compares in real time the number of viewers for the following 3 games:
+
+- Hearthstone https://www.twitch.tv/directory/game/Hearthstone
+- Rocket League https://www.twitch.tv/directory/game/Rocket%20League
+- Dota 2 https://www.twitch.tv/directory/game/Dota%202
+
+**Technology**
+
+You are free to use one or more frameworks or libraries of your choice and will not be penalized for that as long as you can explain it.
+
+- Frontend: Javascript
+- Backend: Javascript
+
+**Rules**
+
+- The project should not take more than 1 week.
+- To submit your project, please push the code in this repository (You'll have access to this repository for a week).
+- Also, please have the project hosted online using a service such as Heroku and provide us with the link.
+- You are encouraged to submit all of your work at the end of the 1 week period even if it is not complete.
+
+**Important**
+
+- Document your API design (routes + payload)
+- Document your code only if needed
+- Don't forget to update your README.md with the steps required to run your project and tests (if any)
+
+Bon courage !
+
+**Step to run the project**
+  npm run devStart or nodemon server.js
+  
---
+++ server.js
@@ -0,0 +1,132 @@
+const express=require('express')
+const app=express()
+const server=require('http').Server(app)
+const request=require('request')
+const axios=require('axios')
+const io=require('socket.io')(server);
+require('dotenv').config();
+
+
+app.set('view engine','ejs')
+app.use(express.static(__dirname+"/public"));
+// Recuperer le token de me compte twtich
+const getTokens=(url,takeResponse)=>{
+    const hearders={
+        url:"https://id.twitch.tv/oauth2/token",
+        json:true,
+        body:{
+            client_id:'5lwqp9tddm2mo4s53bsg5dqbcim3sc',
+            client_secret:'8sqp4yqifo7v5tp3pr30h776tp4eb2',
+            grant_type:'client_credentials'
+        }
+
+
+    };
+    // on Poste les indentifiants et on recupere la reponse
+    request.post(hearders,(err,res,body)=>{
+        if(err) return console.log(err)
+        takeResponse(res);
+    })
+  
+}
+ var Token='';
+ // on accede au token
+ getTokens('https://id.twitch.tv/oauth2/token',(res)=>{
+      Token=res.body.access_token;
+      return Token;
+    
+ })
+ // on se connecte a l'api twitch
+ const getGameChess=async (url,accessToken,callFunction)=>{
+     const ChessOption=axios.create({
+         headers:{
+             'Client-ID':'5lwqp9tddm2mo4s53bsg5dqbcim3sc',
+             'Authorization':'Bearer '+ accessToken
+
+         }
+     });
+     // on recupere les infos de Chess game
+     const resultC = await ChessOption.get('https://api.twitch.tv/helix/games',{params:{
+         name:'Chess'
+     }})
+      // on recupere les infos de hearthstone game
+     const resultH = await ChessOption.get('https://api.twitch.tv/helix/games',{params:{
+        name:'Hearthstone'
+    }})
+     // on recupere les infos de rocket league game
+    const resultR = await ChessOption.get('https://api.twitch.tv/helix/games',{params:{
+        name:'Rocket League'
+    }})
+     // on recupere les infos de dota 2 game
+    const resultD = await ChessOption.get('https://api.twitch.tv/helix/games',{params:{
+        name:'Dota 2'
+    }})
+   
+    // maintenant on utilise les id pour recuperer  les streams pour chacun des jeux 
+     const chessStream= await  ChessOption.get('https://api.twitch.tv/helix/streams',{params:{
+         game_id:resultC.data.data[0].id
+     }})
+     const hearthStream= await  ChessOption.get('https://api.twitch.tv/helix/streams',{params:{
+         game_id:resultH.data.data[0].id
+     }})
+     const rocketStream= await  ChessOption.get('https://api.twitch.tv/helix/streams',{params:{
+         game_id:resultR.data.data[0].id
+     }})
+     const dotaStream= await  ChessOption.get('https://api.twitch.tv/helix/streams',{params:{
+         game_id:resultD.data.data[0].id
+     }})
+     // on appelle notre callback pour passer les data en parametre
+     callFunction( chessStream.data,hearthStream.data,rocketStream.data,dotaStream.data)
+     
+     
+ }
+
+app.get('/',(req,res)=>{
+    res.render('index');
+  
+})
+
+ 
+  function getChessViewer(){
+        // la fonction prend l'url,le token et le callback en parametre
+     getGameChess('https://api.twitch.tv/helix/games/top',Token,(chess,hearth,rocket,dota)=>{
+        
+     //on calcule le nombre de visionneur en fonction du jeux qui sera passer en parametre
+     var viewer=(input)=>{
+        var countStream=0;
+        input.data.map(stream=>{
+            countStream+=stream.viewer_count;
+            
+        }) 
+        return countStream
+        
+
+     }   
+     
+    
+        
+       
+     //notre websocket emet le resultat chque seconde
+       io.sockets.send({chess:viewer(chess),hearth:viewer(hearth),dota:viewer(dota),rocket:viewer(rocket)});
+       
+       
+      
+
+})
+      
+  }
+  setInterval(getChessViewer, 1000);
+  
+
+
+ 
+
+
+    
+
+ 
+ 
+
+server.listen(5000,()=>{
+    console.log("listen to port")
+})
